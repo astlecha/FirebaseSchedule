862481bf1b690762b178ebc3510968c9dd99f9ca
Charlotte Astle <charlotteastle@Charlottes-MacBook-Pro.local>
2017-06-27 09:32:37 -0700

    initializes firebase and js

---
+++ script/logic.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD9dbd-m0gfy3Eyi5v0MtyYB7zsihXlox8",
+    authDomain: "train-schedule-6f8e4.firebaseapp.com",
+    databaseURL: "https://train-schedule-6f8e4.firebaseio.com",
+    projectId: "train-schedule-6f8e4",
+    storageBucket: "train-schedule-6f8e4.appspot.com",
+    messagingSenderId: "936118382220"
+  };
+  firebase.initializeApp(config);
